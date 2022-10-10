795a1e47e5edc982f173ebe305b26a8bd642dd6c
Jérôme Lesaux <jerome.lesaux.ext@ovhcloud.com>
2022-10-10 14:24:33 +0200

    add: add SPR offset function.

--- lib_text.asm
+++ lib_text.asm
@@ -1,7 +1,7 @@
 ;----- recherche de la bonne lettre dans la fonte -----------------------
 ; hl adresse du texte 
 ; bc adresse du pointeur de la fonte
-getSpriteHardPosition
+getSpriteHardOffset
 ld a,(hl)
 sub 65            ; soustrait la premiere lettre ascii de la fonte A 
 
@@ -9,12 +9,13 @@ ld l,a
 ld h,0
 add hl,hl ;*2
 add hl,hl ;*4
+ld b,h 
+ld c,l
 add hl,hl ;*8
 add hl,hl ;*16
 add hl,hl ;*32
 add hl,hl ;*64
-ld de,0004
-add hl,de ; 256 octets taille d'une sprite hard
+add hl,bc ; 256 octets taille d'une sprite hard
 ld de,bc        ; recuperation du pointeur de la font
 add hl,de         ; hl pointe sur la bonne lettre dans la fonte
 ret
@@ -24,6 +25,6 @@ ret
 ; hl pointe sur la lettre dans la fonte
 ; de pointe sur la position du sprite hard
 copyFontSpriteHard 
-ld bc, #ff
+ld bc, #00ff
 ldir
 ret
